3b2136d27a2fee0201c2df6e4509e588bc635614
MaciejKamianowski <maciek-kamianowski@o2.pl>
2023-08-09 20:37:21 +0200

    Added candidates component

--- vote-app-frontend/src/app/app.module.ts
+++ vote-app-frontend/src/app/app.module.ts
@@ -5,18 +5,18 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { MainComponent } from './components/main/main.component';
 import { HeaderComponent } from './components/header/header.component';
+import { CandidatesComponent } from './components/candidates/candidates.component';
+import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
   declarations: [
     AppComponent,
     MainComponent,
-    HeaderComponent
+    HeaderComponent,
+    CandidatesComponent,
   ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
+  imports: [BrowserModule, AppRoutingModule, HttpClientModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
---
+++ vote-app-frontend/src/app/components/candidates/candidates.component.ts
@@ -0,0 +1,28 @@
+import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+
+@Component({
+  selector: 'app-candidates',
+  templateUrl: './candidates.component.html',
+  styleUrls: ['./candidates.component.scss'],
+})
+export class CandidatesComponent implements OnInit {
+  candidates: any[] = [];
+
+  constructor(private http: HttpClient) {}
+
+  ngOnInit(): void {
+    this.fetchCandidates();
+  }
+
+  fetchCandidates(): void {
+    this.http.get<any[]>('http://localhost:8080/candidates/all').subscribe(
+      (data) => {
+        this.candidates = data;
+      },
+      (error) => {
+        console.error('Error fetching candidates:', error);
+      }
+    );
+  }
+}
